0962ec6c1a11ab66015a47fdc988f11ff2a95bee
jralvarino <alvarinojr@gmail.com>
2018-05-09 14:47:40 -0300

    teste

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -13,7 +13,7 @@ public class Cliente {
         this.comprasEfetuadas = new ArrayList<Compra>();
     }
 
-    public  efetuarCompra(Compra compra, double dinheiro) throws FaltouDinheiroException {
+    public double efetuarCompra(Compra compra, double dinheiro) throws FaltouDinheiroException {
         if (compra.possoPagar(dinheiro)) {
             comprasEfetuadas.add(compra);
             return compra.calcularTroco(dinheiro);
